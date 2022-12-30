c8874e3dc631192d4f68146d0b6be9264877e19f
min05ji26 <112680465+min05ji26@users.noreply.github.com>
2022-12-31 00:03:36 +0900

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,132 @@
+import pyautogui
+import time
+
+chrome = pyautogui.locateOnScreen('img/1-1.png')
+pyautogui.doubleClick(chrome);
+
+time.sleep(2.5)
+
+pyautogui.moveTo(1300,700,duration=1)
+pyautogui.doubleClick()
+
+time.sleep(2.5)
+
+key = pyautogui.locateOnScreen('img/2-1.png')
+pyautogui.doubleClick(key);
+
+time.sleep(2.5)
+
+mouse = pyautogui.locateOnScreen('img/2-2.png')
+pyautogui.doubleClick(mouse);
+
+time.sleep(2.5)
+
+english = pyautogui.locateOnScreen('img/2-3.png')
+pyautogui.doubleClick(english);
+
+time.sleep(2.5)
+
+Y = pyautogui.locateOnScreen('img/2-4.png')
+pyautogui.doubleClick(Y);
+
+time.sleep(2.5)
+
+O = pyautogui.locateOnScreen('img/2-5.png')
+pyautogui.doubleClick(O);
+
+time.sleep(2.5)
+
+U = pyautogui.locateOnScreen('img/2-6.png')
+pyautogui.doubleClick(U);
+
+time.sleep(2.5)
+
+T = pyautogui.locateOnScreen('img/2-7.png')
+pyautogui.doubleClick(T);
+
+time.sleep(2.5)
+
+U = pyautogui.locateOnScreen('img/2-8.png')
+pyautogui.doubleClick(U);
+
+time.sleep(2.5)
+
+B = pyautogui.locateOnScreen('img/2-9.png')
+pyautogui.doubleClick(B);
+
+time.sleep(2.5)
+
+E = pyautogui.locateOnScreen('img/2-10.png')
+pyautogui.doubleClick(E);
+
+time.sleep(2.5)
+
+search = pyautogui.locateOnScreen('img/2-11.png')
+pyautogui.doubleClick(search);
+
+time.sleep(2.5)
+
+youtube = pyautogui.locateOnScreen('img/1.jpg')
+pyautogui.doubleClick(youtube);
+
+time.sleep(2.5)
+
+keyboard = pyautogui.locateOnScreen('img/2.jpg')
+pyautogui.doubleClick(keyboard);
+
+time.sleep(2.5)
+
+a = pyautogui.locateOnScreen('img/3.jpg')
+pyautogui.click(a);
+
+time.sleep(0.5)
+
+p = pyautogui.locateOnScreen('img/4.jpg')
+pyautogui.click(p);
+
+time.sleep(0.5)
+
+f = pyautogui.locateOnScreen('img/5.jpg')
+pyautogui.click(f);
+
+time.sleep(0.5)
+
+f = pyautogui.locateOnScreen('img/5.jpg')
+pyautogui.doubleClick(f);
+
+time.sleep(0.5)
+
+h = pyautogui.locateOnScreen('img/6.jpg')
+pyautogui.click(h);
+
+time.sleep(0.5)
+
+s = pyautogui.locateOnScreen('img/7.jpg')
+pyautogui.click(s);
+
+time.sleep(0.5)
+
+c = pyautogui.locateOnScreen('img/8.jpg')
+pyautogui.click(c);
+
+time.sleep(0.5)
+
+k = pyautogui.locateOnScreen('img/9.jpg')
+pyautogui.click(k);
+
+time.sleep(0.5)
+
+x = pyautogui.locateOnScreen('img/10.jpg')
+pyautogui.click(x);
+
+time.sleep(0.5)
+
+m = pyautogui.locateOnScreen('img/11.jpg')
+pyautogui.click(m);
+
+time.sleep(0.5)
+
+search = pyautogui.locateOnScreen('img/12.png')
+pyautogui.doubleClick(search);
+
+time.sleep(2.5)
